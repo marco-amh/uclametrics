c13d3e44ef1f305761ccd474d745d056ddae3e41
marcovaas <50920809+marcovaas@users.noreply.github.com>
2022-06-15 09:57:55 -0500

    Add files via upload

--- GrangerCausality.py
+++ GrangerCausality.py
@@ -40,7 +40,7 @@ def Granger_Causality(df, lags):
     results.rename(columns={0:'X->Y F-calc',1:'X->Y F-critical',2:'X->Y p-value',
                             3:'Y->X F-calc',4:'Y->X F-critical',5:'Y->X p-value'}, inplace=True)
     df1 = df.copy() # x as function of y
-    df2 = df1[df1.columns[::-1]] # y as function of x
+    df2 = df1[df1.columns[::-1]].copy() # y as function of x
     
     j = 0
     for i in range(1,lags+1):
@@ -52,9 +52,9 @@ def Granger_Causality(df, lags):
         ssr1_u, ssr1_u_m = anova(df1_u)               # Anova Eq1 unrestricted
         q1 = ssr1_u_m - ssr1_r_m                      # Number of restrictions
         f1 = ((ssr1_r-ssr1_u)/q1)/(ssr1_u/(n-ssr1_u_m))# F-Test
-    
-        df2_r = lagger_e(p=lags,df = df2.iloc[:,0:1])# Eq2 restricted
-        df2_u = lagger_e(p=lags,df = df2)            # Eq2 unrestricted (all vars)
+
+        df2_r = lagger_e(p=i,df = df2.iloc[:,0:1])   # Eq2 restricted
+        df2_u = lagger_e(p=i,df = df2)               # Eq2 unrestricted (all vars)
     
         ssr2_r, ssr2_r_m = anova(df2_r)              # Anova Eq2 restricted
         ssr2_u, ssr2_u_m = anova(df2_u)              # Anova Eq2 unrestricted
@@ -64,7 +64,7 @@ def Granger_Causality(df, lags):
         F_critical = f.ppf(0.95, ssr1_u_m-1, n-ssr1_u_m)
         
         p_value_f1 = 1- f.cdf(f1, q1,n-ssr1_u_m)
-        p_value_f2 = 1- f.cdf(f2, q2,n-ssr1_u_m)
+        p_value_f2 = 1- f.cdf(f2, q2,n-ssr2_u_m)
         
         results.iloc[j:j+1,0:1] =  f1
         results.iloc[j:j+1,1:2] =  F_critical
@@ -74,11 +74,11 @@ def Granger_Causality(df, lags):
         results.iloc[j:j+1,5:6] =  p_value_f2
         
         j=j+1
-    #print('Granger Causality Test \n'
-    #      'Granger statistic is ((SSE_r-SSE_u)/q) / ((SSE_u)/(n-k))  \n'
-    #      'Ho: b_1=b_2=...=0. X does not Granger cause Y.\n'
-    #      'Ha: X Granger causes Y.\n'
-    #      'We reject Ho if p-value < 0.05 ')
+    print('Granger Causality Test \n'
+          'Granger statistic is ((SSE_r-SSE_u)/q) / ((SSE_u)/(n-k))  \n'
+          'Ho: b_1=b_2=...=0. X does not Granger cause Y.\n'
+          'Ha: X Granger causes Y.\n'
+          'We reject Ho if p-value < 0.05 ')
     
     return results
     
@@ -87,4 +87,5 @@ url = 'https://github.com/marcovaas/uclametrics/blob/main/Data.xlsx?raw=true'
 df = pd.read_excel(url, sheet_name = 'ols', index_col = 0)
 df = df.iloc[:,0:2]
 a = Granger_Causality(df=df, lags=3)
+aa = Granger_Causality(df=df, lags=4)
 '''
